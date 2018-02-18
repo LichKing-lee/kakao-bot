32f5dab76ba9f2b3b2aa5d1a2c7a947f83fe2a67
ChangYong <lcy9002@naver.com>
2018-02-18 14:49:56 +0900

    Refactor

--- src/main/java/com/yong/kakaobot/api/skt/SktService.kt
+++ src/main/java/com/yong/kakaobot/api/skt/SktService.kt
@@ -11,9 +11,21 @@ class SktService: ApiService {
     @Resource(name = "sktApiCaller")
     private lateinit var apiCaller: ApiCaller
 
+    companion object {
+        val DEFAULT_PARAMS: Map<String, String> = mapOf("city" to "서울", "county" to "동작구", "village" to "사당동")
+    }
+
     override fun callToApi(keyword: String): String {
-        val params: Map<String, String> = mapOf("city" to "서울", "county" to "동작구", "village" to "사당동")
+        val params: Map<String, String> = createParams(keyword.split(" "))
         val response: WeatherResponse = apiCaller.callForObject("https://api2.sktelecom.com/weather/current/hourly", params, WeatherResponse::class.java)
         return response.toString()
+    }
+
+    private fun createParams(keywords: List<String>): Map<String, String> {
+        if(keywords.count() < 4) {
+            return DEFAULT_PARAMS
+        }
+
+        return mapOf("city" to keywords[1], "county" to keywords[2], "village" to keywords[3])
     }
 }
--- src/main/java/com/yong/kakaobot/api/skt/weather/Hourly.kt
+++ src/main/java/com/yong/kakaobot/api/skt/weather/Hourly.kt
@@ -3,6 +3,7 @@ package com.yong.kakaobot.api.skt.weather
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Hourly(val grid: Grid,
@@ -10,5 +11,7 @@ data class Hourly(val grid: Grid,
                   private val sky: Sky,
                   private val temperature: Temperature,
                   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") private val timeRelease: LocalDateTime) {
-    override fun toString(): String = "$grid\n$precipitation\n$sky\n$temperature\n갱신시각 : $timeRelease"
+    override fun toString(): String = "$grid\n$precipitation\n$sky\n$temperature\n갱신시각 : ${dateFormatting()}"
+
+    private fun dateFormatting(): String = timeRelease.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
 }
